5e6e24c0d7331d8a7283a37054c65ffe0f9c16d6
prakash-s-2210 <sbprakash.kvs@gmail.com>
2023-12-26 02:38:11 +0530

    added host name to nextjs.config

--- app/projects/[id]/widget-configuration/page.jsx
+++ app/projects/[id]/widget-configuration/page.jsx
@@ -3,12 +3,13 @@ import {
   ProjectNavbar,
   WidgetConfigurationForm,
 } from "../../../../components/index";
+import Image from "next/image";
 
 import { fetchProjectById } from "../../../../lib/actions/project.actions";
 
 const Page = async ({ params, searchParams }) => {
   const project = await fetchProjectById(params.id);
-
+console.log(project.widgetConfiguration.botIcon)
   return (
     <main className="flex">
       <Sidebar id={project._id} title={project.title} />
@@ -18,12 +19,18 @@ const Page = async ({ params, searchParams }) => {
 
         <div className="px-8 md:px-16 flex flex-col gap-10">
           <h1 className="text-[55px] font-bold text-primary">Configuration</h1>
-
-          <WidgetConfigurationForm
+          <Image
+                    src={project.widgetConfiguration.botIcon}
+                    alt="Bot icon"
+                    width={48}
+                    height={48}
+                    className="object-contain rounded-full w-12 h-12"
+                  />
+          {/* <WidgetConfigurationForm
             projectId={project._id}
             tab={searchParams.tab}
             widgetConfiguration = {project?.widgetConfiguration}
-          />
+          /> */}
         </div>
       </section>
     </main>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   experimental: { 
     serverComponentsExternalPackages: ["mongoose"] },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+        },
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+        },
+        {
+          hostname: "utfs.io",
+        },
+      ],
+    },
 };
 
 module.exports = nextConfig;
