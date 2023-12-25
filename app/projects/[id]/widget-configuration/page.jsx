590ee143a44f7c6e40703ffa4e1a4a54d2891bd5
prakash-s-2210 <sbprakash.kvs@gmail.com>
2023-12-26 02:38:59 +0530

    added host name to nextjs.config

--- app/projects/[id]/widget-configuration/page.jsx
+++ app/projects/[id]/widget-configuration/page.jsx
@@ -3,13 +3,12 @@ import {
   ProjectNavbar,
   WidgetConfigurationForm,
 } from "../../../../components/index";
-import Image from "next/image";
 
 import { fetchProjectById } from "../../../../lib/actions/project.actions";
 
 const Page = async ({ params, searchParams }) => {
   const project = await fetchProjectById(params.id);
-console.log(project.widgetConfiguration.botIcon)
+
   return (
     <main className="flex">
       <Sidebar id={project._id} title={project.title} />
@@ -19,18 +18,12 @@ console.log(project.widgetConfiguration.botIcon)
 
         <div className="px-8 md:px-16 flex flex-col gap-10">
           <h1 className="text-[55px] font-bold text-primary">Configuration</h1>
-          <Image
-                    src={project.widgetConfiguration.botIcon}
-                    alt="Bot icon"
-                    width={48}
-                    height={48}
-                    className="object-contain rounded-full w-12 h-12"
-                  />
-          {/* <WidgetConfigurationForm
+
+          <WidgetConfigurationForm
             projectId={project._id}
             tab={searchParams.tab}
             widgetConfiguration = {project?.widgetConfiguration}
-          /> */}
+          />
         </div>
       </section>
     </main>
